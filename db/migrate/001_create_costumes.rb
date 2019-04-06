5871b53b4a0d508383e0c59e8cca67b251834ac4
Meisuleen Kased <meisuleen@gmail.com>
2019-04-06 02:42:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :name
       t.float  :price
       t.string :image_url
-      t.integar :size
+      t.integer :size
       t.datetime :created_at
       t.datetime :updated_at
     end

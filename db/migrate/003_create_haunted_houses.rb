8b3f90eac16243375e575d222deea49a2f3274d3
Meisuleen Kased <meisuleen@gmail.com>
2019-04-06 02:41:00 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,15 @@
 # Create your haunted_houses migration here
-class HauntedHouse < ActiveRecord::Migration[4.2]
+class HauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.text   :location
+      t.string :theme
+      t.integar :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
+    end
+  end
 end

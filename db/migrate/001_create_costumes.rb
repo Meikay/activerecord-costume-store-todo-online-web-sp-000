fc68ed139bef58a2af6387d6754b8bb5b84904d2
Meisuleen Kased <meisuleen@gmail.com>
2019-04-06 02:24:24 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,14 +9,14 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
-  # def change
-  #   create_table :costumes do |t|
-  #     t.string :name
-  #     t.string :price
-  #     t.string :image_url
-  #     t.string :time
-  #     t.string :update
-  #
-  #   end
-  # end
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :price
+      t.string :image_url
+      t.string :size
+      t.string :created_at
+      t.string :updated_at
+    end
+  end
 end
